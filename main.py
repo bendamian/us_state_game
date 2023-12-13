5a4420fb7d039931294428c6c6e399685e13188f
bendamian <sridamian@gmail.com>
2023-12-13 17:51:11 +0000

    commit no-4

--- main.py
+++ main.py
@@ -12,7 +12,15 @@ all_states = data.state.to_list()
 guessed_state = []
 
 while len(guessed_state) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 Correct", prompt="Enter a state name?")
+    answer_state = screen.textinput(title=f"{len(guessed_state)}/50 Correct", prompt="Enter a state name?").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_state:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("missing_states.csv")
+        break
 
     if answer_state in all_states:
         guessed_state.append(answer_state)
@@ -23,4 +31,4 @@ while len(guessed_state) < 50:
         t.goto(int(state_data.x), int(state_data.y))
         t.write(state_data.state.item())
 
-turtle.mainloop()
+# turtle.mainloop()
